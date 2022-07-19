0cf3478992eb33c89fc728837fcaa062f0f936b5
sadebare <adebaresulaimon04@gmail.com>
2022-07-19 21:01:29 +0100

    task 2 - selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort an array by selection
+ *
+ * @array: list to be sorted
+ * @size: length of the array
+ *
+ * Return: sorted list
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, temp, min;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
