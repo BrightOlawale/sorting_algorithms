fa0cb1f77ce0648ff8dd2e49ae8d6dba770c602a
Bright Olawale <oolatunjibright@yahoo.com>
2022-07-19 12:55:13 +0100

    [doc #5]: insertion sort added

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,37 +10,40 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *past;
 
-    if (!list || !*list)
-        return;
 
-    current = *list;
-    current = current->next;
+	listint_t *current, *past;
 
-    while (current)
-    {
 
-        while (current->prev && (current->n) < (current->prev->n))
-        {
-            past = current->prev;
-            if (current->next)
-                (current->next)->prev = past;
+	if (!list || !*list)
+	{
+		return;
+	}
+	current = *list;
+	current = current->next;
 
-            if (past->prev)
-            {
-                past->prev->next = current;
-            }
-            else
-            {
-                *list = current;
-            }
-            past->next = current->next;
-            current->prev = past->prev;
-            past->prev = current;
-            current->next = past;
-            print_list(*list);
-        }
-        current = current->next;
-    }
+	while (current)
+	{
+		while (current->prev && (current->n) < (current->prev->n))
+		{
+			past = current->prev;
+			if (current->next)
+				(current->next)->prev = past;
+
+			if (past->prev)
+			{
+				past->prev->next = current;
+			}
+			else
+			{
+				*list = current;
+			}
+			past->next = current->next;
+			current->prev = past->prev;
+			past->prev = current;
+			current->next = past;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
